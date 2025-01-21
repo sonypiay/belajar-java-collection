3be5e0c2d3dc2842997914225332f6364dc42ae2
sonypiay <>
2025-01-22 01:51:34 +0700

    enum set

---
+++ src/programmer/zaman/now/collection/data/EnumSetApp.java
@@ -0,0 +1,19 @@
+package programmer.zaman.now.collection.data;
+
+import java.util.EnumSet;
+
+public class EnumSetApp {
+    public static enum Gender {
+        MALE,
+        FEMALE,
+        NOT_MENTION
+    }
+
+    public static void main(String[] args) {
+        EnumSet<Gender> genders = EnumSet.allOf(Gender.class);
+
+        for (var gender : genders) {
+            System.out.println(gender);
+        }
+    }
+}
