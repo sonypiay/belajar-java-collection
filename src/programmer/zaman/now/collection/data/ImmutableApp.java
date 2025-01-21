095ceceb55c28d5104a2868edd218d1fa9c8174c
sonypiay <>
2025-01-22 01:40:25 +0700

    immutable

---
+++ src/programmer/zaman/now/collection/data/ImmutableApp.java
@@ -0,0 +1,19 @@
+package programmer.zaman.now.collection.data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ImmutableApp {
+    public static void main(String[] args) {
+        List<String> one = Collections.singletonList("satu");
+        List<String> empty = Collections.emptyList();
+        List<String> mutable = new ArrayList<>();
+
+        mutable.add("Sony");
+        mutable.add("Eko");
+
+        List<String> immutable = Collections.unmodifiableList(mutable);
+        List<String> elements = List.of("Sony", "Darmawan");
+    }
+}
