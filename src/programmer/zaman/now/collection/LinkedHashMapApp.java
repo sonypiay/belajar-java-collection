896603a59e018d40ead481fb6b38919d807e72be
sonypiay <pyscho30@gmail.com>
2025-02-13 02:37:46 +0700

    linked hash map

---
+++ src/programmer/zaman/now/collection/LinkedHashMapApp.java
@@ -0,0 +1,20 @@
+package programmer.zaman.now.collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class LinkedHashMapApp {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("first", "Eko");
+        map.put("last", "Khannedy");
+        map.put("middle", "Kurniawan");
+
+        Set<String> keys = map.keySet();
+        for (var key : keys) {
+            System.out.println(key);
+        }
+    }
+}
